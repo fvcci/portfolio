5061bdfaf8f2ab9b2030800a020ce4c1675defc3
Felix Fong <fongf2@mcmaster.ca>
2024-12-07 20:20:55 -0500

    feat: set default font to jetbrains mono

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
-import { GeistSans } from "geist/font/sans";
 import { type AppType } from "next/app";
+import { JetBrains_Mono } from "next/font/google";
 
 import "~/styles/globals.css";
 
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-jetbrains-mono",
+});
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <div className={GeistSans.className}>
+    <div className={`${jetbrainsMono.className} bg-[#FAFAFA]`}>
       <Component {...pageProps} />
     </div>
   );
